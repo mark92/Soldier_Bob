15901abb12fc76ba4aa3b76897f7e011984418f9
Erik <erik.kisel@gmail.com>
2013-12-07 18:15:49 +0200

    vectorz + explosion object instantiate
    @rocketBounce.js

--- Assets/Scripts/bounceRocket.js
+++ Assets/Scripts/bounceRocket.js
@@ -1,7 +1,8 @@
 ﻿#pragma strict
 
 var explosionSound : AudioClip;
-var collided : boolean;
+var reflectMultiplier : float; //intensity of bounce
+var explosionSprite : GameObject;
 
 function Start () {    
 }
@@ -13,13 +14,31 @@ function Update () {
 function FixedUpdate () {  
 }
 
-function OnCollisionEnter2D(coll : Collision2D) {
-    if (coll.collider.tag != "helmet") {
+function OnCollisionExit2D(coll : Collision2D) {
+    if (coll.collider.tag == "helmet") {
     	Debug.Log(coll.collider.tag);
-    	audio.PlayOneShot(explosionSound);
-        // var direction : Vector2 = rigidbody2D.velocity;
-        // var mag : float = rigidbody2D.velocity.magnitude;
-        // rigidbody2D.AddForce(Vector3.Reflect(direction, collisionInfo.contacts[0].normal) * mag, ForceMode.Impulse);
+    	var normal : Vector2 = coll.contacts[0].normal;
+    	var direction : Vector2 = rigidbody2D.velocity;
+    	var reflection : Vector2;
+    	reflection = 2 * Vector2.Dot(direction, normal) * normal - direction;    	
+    	Debug.DrawRay(coll.contacts[0].point, coll.contacts[0].normal, Color.blue, 10);
+    	Debug.DrawRay(coll.contacts[0].point, direction, Color.red, 10);
+    	Debug.DrawRay(coll.contacts[0].point, reflection, Color.green, 10);
+    	// rigidbody2D.AddForce(reflection);
+    	rigidbody2D.velocity = reflection * reflectMultiplier;
+    	rigidbody2D.fixedAngle = true;
     }
+    else
+    {
+        audio.PlayOneShot(explosionSound);
+        // WaitForSeconds(2);
+        // var explosion : GameObject = Instantiate();
+        // explosionSprite.transform.parent = this.gameObject.transform;
+        // Destroy(this.gameObject);
+    }
+}
+
+function OnBecameInvisible() {  //just in case
+    Destroy(this.gameObject); 
 }
         
